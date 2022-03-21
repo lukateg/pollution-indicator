579f6f840f22e73ea203c64d3ca6be6e521cb8b4
Luka <88896073+lukateg@users.noreply.github.com>
2022-03-21 12:57:07 +0100

    Update airVisualsAPI.js

--- src/services/airVisualsAPI.js
+++ src/services/airVisualsAPI.js
@@ -1,13 +1,9 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const airVisualsApiHeaders = {
-//   "x-rapidapi-host": "airvisual1.p.rapidapi.com",
-//   "x-rapidapi-key": "6fb548b82dmsh316467150eb8751p1c3636jsn4ce316da1edb",
-// };
 const airVisualsApiHeaders = {
   "x-rapidapi-host": "airvisual1.p.rapidapi.com",
-  "x-rapidapi-key": "89c4b250e6msh9a3e0d6e03d99a5p13fb26jsn1029d3a245bb",
+  "x-rapidapi-key": "---------------------------------------",
 };
 const baseUrl = "https://airvisual1.p.rapidapi.com/v2";
 
